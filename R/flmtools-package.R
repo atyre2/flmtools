700d017600ae01abd19009c90a5459acb51aaf53
Drew Tyre <atyre2@unl.edu>
2019-06-10 16:27:04 -0500

    add some packages and package documentation

---
+++ R/flmtools-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+#' @importFrom dplyr common_by_from_vector
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
