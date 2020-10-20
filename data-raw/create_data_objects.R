8dd8f9ef69c28ce9e0ab7ce8954975c9eb663e18
Drew Tyre <atyre2@unl.edu>
2020-10-20 15:20:25 -0500

    force test data to be in data.frame objects

--- data-raw/create_data_objects.R
+++ data-raw/create_data_objects.R
@@ -2,5 +2,7 @@
 env_data <- readr::read_csv("data-raw/env_data.csv")
 response_data <- readr::read_csv("data-raw/response_data.csv")
 test_6_24 <- readRDS("data-raw/test_6_24.RDS")
+env_data <- as.data.frame(env_data)
+response_data <- as.data.frame(response_data)
 usethis::use_data(env_data, response_data, overwrite = TRUE)
 usethis::use_data(test_6_24, overwrite = TRUE, internal = TRUE)
